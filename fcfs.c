7c0c2df4024c048c00fe064d77d4ea7bcee4a8fd
StavrianopoulouMaria <stavrianopoulou.maria@gmail.com>
2022-11-21 23:40:24 +0200

    implement fcfs

--- fcfs.c
+++ fcfs.c
@@ -5,8 +5,24 @@ typedef struct {
 	int pid;
 	int arrival_time;
 	int burst_time;
+
+	int start_time;	// time when the process starts executing
+	int end_time;	// time when the process ends executing
 } process;
 
+// implementation of the fcfs algorithm
+void execute_fcfs(process *processes, int n) {
+	if (n <= 0)
+		return;
+
+	processes[0].start_time = 0;
+	processes[0].end_time = processes[0].burst_time;
+	
+	for (int i = 1; i < n; i++) {
+		processes[i].start_time = processes[i - 1].end_time;
+		processes[i].end_time = processes[i].start_time + processes[i].burst_time;
+	}
+}
 
 int main() {
 
@@ -24,8 +40,11 @@ int main() {
 	}
 	/* read in data - DO NOT EDIT (END) */
 
-
-
+	execute_fcfs(arr, n);
+	printf("Process =============== Start time - End time\n");
+	for (int i = 0; i < n; i++) {
+		printf("P%d								%d - %d\n", i + 1, arr[i].start_time, arr[i].end_time);
+	}
 
 	return 0; /* DO NOT EDIT THIS LINE */
 }
