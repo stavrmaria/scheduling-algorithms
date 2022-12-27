abdb04d65aac7efc0b0ae93350377c91a2828231
Stavrainopoulou Maria <maria@pop-os.localdomain>
2022-12-27 21:42:41 +0200

    implement round robin

--- rr.c
+++ rr.c
@@ -5,57 +5,107 @@ typedef struct {
 	int pid;
 	int arrival_time;
 	int burst_time;
-
-	int waiting_time;
-	int turnaround_time;
-	int remaining_time;
 } process;
 
-void calculate_times(process *processes, int n) {
-	processes[0].waiting_time = 0;
-	processes[0].turnaround_time = processes[0].burst_time;
-	processes[0].remaining_time = processes[0].burst_time;
-
-	for (int i = 1; i < n; i++) {
-		processes[i].waiting_time = processes[i - 1].burst_time + processes[i - 1].waiting_time;
-		processes[i].turnaround_time = processes[i].waiting_time + processes[i].burst_time;
-		processes[i].remaining_time = processes[i].burst_time;
+int search(process *processes, int n, int pid) {
+	for (int i = 0; i < n; i++) {
+		if (processes[i].pid == pid)
+			return i;
 	}
+	
+	return -1;
 }
 
-void execute_fcfs(process *processes, int n, int quantum) {
-	int index = 0;
-	int next_index = 1;
+process *rotate(process *processes, int n) {
+	process temp = processes[0];
+	int k = 0;
+
+	for (int i = 0; i < n - 1; i++) {
+		processes[i] = processes[i + 1];
+	}
+
+	processes[n - 1] = temp;
+}
+
+process *add(process *processes, int n, process element) {
+	process* temp = malloc((n + 1) * sizeof(process));
+
+	for (int i = 0; i < n; i++)
+		temp[i] = processes[i];
+	
+	temp[n] = element;
+	processes = (process*)realloc(processes, n + 1);
+
+	for (int i = 0; i < n + 1; i++)
+		processes[i] = temp[i];
+
+	return processes;
+}
+
+process *delete(process *processes, int n, process element) {
+	int index = search(processes, n, element.pid);
+
+	if (n - 1 == 0)
+		return NULL;
+
+	if (index == -1)
+		return processes;
+
+	process* temp = malloc(n * sizeof(process));
+
+	for (int i = 0; i < n; i++)
+		temp[i] = processes[i];
+	
+	processes = (process*)realloc(processes, sizeof(process) * (n - 1));
+	for (int i = 0; i <= index; i++)
+		processes[i] = temp[i];
+	
+	for (int i = index; i < n - 1; i++)
+		processes[i] = temp[i + 1];
+	
+	return processes;
+}
+
+void execute_rr(process *processes, int n, int quantum) {
 	int time;
 	int finished_processes = 0;
+	int m = n;
 	int current_time = processes[0].arrival_time;
-	char pending_process;
+	process *queue = NULL;
+	int current = 0;
+	int size = 0;
+	int current_pid;
+	
+	while (finished_processes < m) {
+		// find processes than have arrived until the current time
+		int counter = 0;
+		while (processes[counter].arrival_time <= current_time && counter < n) {
+			if (search(queue, size, processes[counter].pid) == -1 && processes[counter].burst_time > 0)
+				queue = add(queue, size++, processes[counter]);
 
-	calculate_times(processes, n);
-	while (finished_processes < n) {
-		pending_process = 1;
-
-		if (processes[index].remaining_time > quantum) {
-			processes[index].remaining_time -= quantum;
-			current_time += quantum;
-			time = quantum;
-			next_index = index;
-			next_index = (index + 1) % n;
-		} else {
-			current_time += processes[index].remaining_time;
-			time = processes[index].remaining_time;
-			processes[index].remaining_time = 0;
-			finished_processes++;
-			next_index = 0;
-			while (processes[next_index].remaining_time == 0 && next_index < n) {
-				next_index++;
-			}
+			counter++;
 		}
-		
-		for (int i = 0; i < time; i++)
-			printf("%d\n", processes[index].pid);
 
-		index = next_index;
+		current = (current + 1) % size;
+		if (queue[current].burst_time > quantum) {
+			time = quantum;
+			queue[current].burst_time -= quantum;
+			current_pid = queue[current].pid;
+			queue = rotate(queue, size);
+		} else {
+			time = queue[current].burst_time;
+			queue[current].burst_time = 0;
+			current_pid = queue[current].pid;
+			queue = delete(queue, size--, queue[current]);
+			process element = processes[search(processes, n, current_pid)];
+			processes = delete(processes, n--, element);
+			finished_processes++;
+		}
+
+		current_time += time;
+
+		for (int i = 0; i < time; i++)
+			printf("%d\n", current_pid);
 	}
 }
 
@@ -75,7 +125,7 @@ int main() {
 	}
 	/* read in data - DO NOT EDIT (END) */
 	
-	execute_fcfs(arr, n, quantum);
+	execute_rr(arr, n, quantum);
 	free(arr);
 
 	return 0; /* DO NOT EDIT THIS LINE */
